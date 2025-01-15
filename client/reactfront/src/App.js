081bd56ce20ed1c1ab2b7365aeb134c3fb836a19
mohammed alsamaki <samkimohd@gmail.com>
2025-01-15 13:47:14 +0300

    cart logic

--- client/reactfront/src/App.js
+++ client/reactfront/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import * as React from 'react';
 
 import {BrowserRouter, Link, Route, Routes} from'react-router-dom';
 
@@ -6,8 +7,221 @@ import Home from './componants/home';
 import Login from './componants/loginCheck';
 import Orders from './componants/orders';
 import logo from './logo.svg';
+import { extendTheme, styled } from '@mui/material/styles';
+import { useEffect, useState } from 'react';
 
-function App() {
+import { AppProvider } from '@toolpad/core/AppProvider';
+import BarChartIcon from '@mui/icons-material/BarChart';
+import DashBoard from './componants/dashBoard';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import { DashboardLayout } from '@toolpad/core/DashboardLayout';
+import DescriptionIcon from '@mui/icons-material/Description';
+import ExitToAppIcon from '@mui/icons-material/ExitToApp';
+import FullFeaturedCrudGrid from './componants/user';
+import Grid from '@mui/material/Grid2';
+import LayersIcon from '@mui/icons-material/Layers';
+import { PageContainer } from '@toolpad/core/PageContainer';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import axios from 'axios';
+import { useNavigate } from'react-router-dom';
+const demoTheme = extendTheme({
+  colorSchemes: { light: true, dark: true },
+  colorSchemeSelector: 'class',
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 600,
+      lg: 1200,
+      xl: 1536,
+    },
+  },
+});
+
+function useDemoRouter(initialPath) {
+  const [pathname, setPathname] = React.useState(initialPath);
+  if(pathname =='/LogOut'){
+    console.log('logging out')
+    sessionStorage.removeItem('jwt')
+    window.location.href = "/login"
+  }
+
+  const router = React.useMemo(() => {
+    return {
+      pathname,
+      searchParams: new URLSearchParams(),
+      navigate: (path) => setPathname(String(path)),
+    };
+  }, [pathname]);
+
+  return router;
+}
+
+const Skeleton = styled('div')(({ theme, height }) => ({
+  backgroundColor: theme.palette.action.hover,
+  borderRadius: theme.shape.borderRadius,
+  height,
+  content: '" "',
+}));
+function App(props) {
+  const { window } = props;
+    const allPages  = [{
+      path:'/dashboard',
+      componant:<DashBoard />,
+    },
+    {
+      path:'/orders',
+      componant:<Orders />,
+    },
+    {
+      path:'/users',
+      componant:<FullFeaturedCrudGrid />,
+    },
+  ];
+  const [CurrentComponant,setCurrentComponant] = useState(allPages[0].componant);
+    const [user, setUsers] = useState([]);
+  // const navigate = useNavigate();
+  const [navigationDash, setNavigationDash] = useState([
+    {
+      kind: 'header',
+      title: 'Main items',
+    },
+    {
+      segment: 'dashboard',
+      title: 'Dashboard',
+      icon: <DashboardIcon />,
+    },
+    {
+      segment: 'orders',
+      title: 'Orders',
+      icon: <ShoppingCartIcon />,
+    },
+    {
+      segment: 'orders',
+      title: 'Orders',
+      icon: <ShoppingCartIcon />,
+    },
+    {
+      segment: 'LogOut',
+      title: 'LogOut',
+      icon: <ExitToAppIcon onClick={()=>{
+        console.log('logout');
+      }} />,
+    },
+
+
+
+  ]);
+  const handelLogOut = ()=>{
+    sessionStorage.removeItem('jwt');
+    // navigate('/login')
+  }
+  const router = useDemoRouter('/dashboard');
+
+  useEffect(()=>{
+    // alert('fetching users');
+    // if (!sessionStorage.getItem('jwt')){
+    //     navigate('/login')
+    // }
+    const token = sessionStorage.getItem('jwt');
+    // axios.defaults.headers.common['Auth'] = `Bearer ${token}`;
+
+    const validateToken = async ()=>{
+        try{
+            const res = await axios.get('http://127.0.0.1:5001/api/home',{
+                headers: {
+                    'Auth': 'Bearer '+token
+                }
+            })
+            console.log("token",res.data);
+            setUsers(res.data.user)
+            if (res.data.user.roul==='admin'){
+                console.log('admin')
+                setNavigationDash(
+                    [
+                        {
+                          kind: 'header',
+                          title: 'Main items',
+                        },
+                        {
+                          segment: 'dashboard',
+                          title: 'Dashboard',
+                          icon: <DashboardIcon />,
+                        },
+                        {
+                          segment: 'orders',
+                          title: 'Orders',
+                          icon: <ShoppingCartIcon />,
+                        },
+                        {
+                          segment: 'users',
+                          title: 'Users',
+                          icon: <ShoppingCartIcon />,
+                        },
+                        {
+                          kind: 'divider',
+                        },
+                        {
+                          kind: 'header',
+                          title: 'Analytics',
+                        },
+                        {
+                          segment: 'reports',
+                          title: 'Reports',
+                          icon: <BarChartIcon />,
+                          children: [
+                            {
+                              segment: 'sales',
+                              title: 'Sales',
+                              icon: <DescriptionIcon />,
+                            },
+                            {
+                              segment: 'traffic',
+                              title: 'Traffic',
+                              icon: <DescriptionIcon />,
+                            },
+                          ],
+                        },
+                        {
+                          segment: 'integrations',
+                          title: 'Integrations',
+                          icon: <LayersIcon />,
+                        },
+                        {
+                          segment: 'LogOut',
+                          title: 'LogOut',
+                          onClick:handelLogOut,
+                          icon: <div onClick={()=>{console.log('logOut')}}>
+                            <ExitToAppIcon />
+                          </div>,
+
+
+                        },
+                      ],
+
+                )
+            }
+
+        }
+        catch(err){
+            console.log(err);
+            if (err.response.status === 401){
+                // navigate('/login')
+            }
+        }
+    }
+
+    validateToken()
+    console.log(router)
+    setCurrentComponant(allPages.find((page) => page.path === router.pathname)?.componant)
+
+   },[router])
+
+
+  // Remove this const when copying and pasting into your project.
+  const demoWindow = window ? window() : undefined;
+
+
   return (
     <div className="App">
       <BrowserRouter>
@@ -18,6 +232,24 @@ function App() {
       </Routes>
       </BrowserRouter>
      {/* <Login/> */}
+     {/*  inline if to do this logic if the path is login didint display appproviders else display app provider */}
+
+     
+
+      <AppProvider
+           navigation={navigationDash}
+           router={router}
+           theme={demoTheme}
+           window={demoWindow}
+         >
+           <DashboardLayout>
+             <PageContainer>
+               {CurrentComponant}
+             </PageContainer>
+
+
+           </DashboardLayout>
+         </AppProvider>
     </div>
   );
 }
--- client/reactfront/src/componants/orders.js
+++ client/reactfront/src/componants/orders.js
@@ -1,44 +1,77 @@
 import * as React from 'react';
-
+import { useState } from 'react';
+import { useEffect } from 'react';
 import { DataGrid } from '@mui/x-data-grid';
 import Paper from '@mui/material/Paper';
+import axios from 'axios';
 
 const columns = [
     { field: 'id', headerName: 'ID', width: 70 },
-    { field: 'useName', headerName: 'useName', width: 130 },
-    { field: 'items', headerName: 'items', width: 130 },
+    { field: 'category', headerName: 'category', width: 130 },
+    { field: 'name', headerName: 'name', width: 130 },
     { field: 'price', headerName: 'price', width: 130 },
 
 
   ];
   const rows = [
-    { id: 1, useName: 'Snow', items: 'Jon', price: 35 },
-    { id: 2, useName: 'Lannister', items: 'Cersei', price: 42 },
-    { id: 3, useName: 'Lannister', items: 'Jaime', price: 45 },
-    { id: 4, useName: 'Stark', items: 'Arya', price: 16 },
-    { id: 5, useName: 'Targaryen', items: 'Daenerys', price: 100 },
-    { id: 6, useName: 'Melisandre', items: null, price: 150 },
-    { id: 7, useName: 'Clifford', items: 'Ferrara', price: 44 },
-    { id: 8, useName: 'Frances', items: 'Rossini', price: 36 },
-    { id: 9, useName: 'Roxie', items: 'Harvey', price: 65 },
+    { id: 1, category: 'Snow', name: 'Jon', price: 35 },
+    { id: 2, category: 'Lannister', name: 'Cersei', price: 42 },
+    { id: 3, category: 'Lannister', name: 'Jaime', price: 45 },
+    { id: 4, category: 'Stark', name: 'Arya', price: 16 },
+    { id: 5, category: 'Targaryen', name: 'Daenerys', price: 100 },
+    { id: 6, category: 'Melisandre', name: null, price: 150 },
+    { id: 7, category: 'Clifford', name: 'Ferrara', price: 44 },
+    { id: 8, category: 'Frances', name: 'Rossini', price: 36 },
+    { id: 9, category: 'Roxie', name: 'Harvey', price: 65 },
   ];
   const getTotalPrice = (rows) => rows.reduce((acc, row) => acc + (row.price || 0), 0);
 const totalPrice = getTotalPrice(rows);
   const paginationModel = { pprice: 0, ppriceSize: 5 };
 
 export default function Orders() {
+  const [cart, setCart] = useState([]);
+  useEffect(() => {
+    const fetchCart = async () => {
+      const token = sessionStorage.getItem('jwt');
+      try {
+        const res = await axios.get('http://127.0.0.1:5001/api/getCart',
+          {
+            headers:{
+              'Auth': 'Bearer '+token
+            }
+          }
+
+        );
+        console.log(res.data.products);
+        setCart(res.data.products);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    fetchCart();
+  }, []);
+
     return(
         <>
-        <h1>Orders </h1>
+        <h1>Cart </h1>
         <Paper sx={{ height: 400, width: '100%' }}>
-      <DataGrid
-        rows={rows}
+      {/* <DataGrid
+        rows={cart}
         columns={columns}
         initialState={{ pagination: { paginationModel } }}
         ppriceSizeOptions={[5, 10]}
         checkboxSelection
         sx={{ border: 0 }}
-      />
+      /> */}
+      {cart.map((item,index) => (
+        <>
+        <h1>{item.productId._id}</h1>
+        <h1>{item.productId.name}</h1>
+        <h1>{item.productId.price}</h1>
+
+        <h1>{item.quantity}</h1>
+        </>
+      ))}
     </Paper>
     <h1>this is the toal price {totalPrice} $</h1>
         </>
